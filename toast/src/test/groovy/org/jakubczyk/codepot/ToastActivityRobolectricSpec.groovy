7a9031ff069880451999f22f2783dd3b52d42cf7
mariopce <mariopce@gmail.com>
2015-08-28 09:46:06 +0200

    my changes

--- toast/src/test/groovy/org/jakubczyk/codepot/ToastActivityRobolectricSpec.groovy
+++ toast/src/test/groovy/org/jakubczyk/codepot/ToastActivityRobolectricSpec.groovy
@@ -5,28 +5,32 @@ import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowToast
 import pl.polidea.robospock.RoboSpecification
 
-@Config(manifest = "./toast/src/main/AndroidManifest.xml")
+@Config(manifest = "./src/main/AndroidManifest.xml")
 class ToastActivityRobolectricSpec extends RoboSpecification {
 
     def "should assign listener to the label"() {
         given: "create activity controller"
-
+        def controller = Robolectric.buildActivity(ToastActivity)
         and: "get the activity from controller"
-
+        def activity = controller.get();
         when: "make controller call onCreate"
-
-        then: "check if correct text is set"
+        controller.create()
+        then: "check if listener is set"
+        def shadow =  Robolectric.shadowOf(activity.labelTv);
+        shadow.onClickListener
     }
 
     def "should show a toast on click event"() {
         given: "create activity controller"
-
+        def controller = Robolectric.buildActivity(ToastActivity)
         and: "get the activity from controller"
-
+        def activity = controller.get();
         and: "make controller call onCreate"
-
+        controller.create()
         when: "call action, click on the label"
-
+        activity.labelTv.performClick();
         then: "check if correct text in the Toast is set"
+        ShadowToast.latestToast =~ "Clicked!"
+        //1* activity.toaster.show("Clicked!")
     }
 }
